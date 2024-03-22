7a9dada28c4e9c2291f18d18ec21c7baaf5995bc
KrystofMaxa <94890860+KrystofMaxa@users.noreply.github.com>
2024-03-22 11:25:06 +0100

    Add files via upload

---
+++ filmy/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from filmy.models import Movie
+
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Movie, MovieAdmin) 
+
+# Register your models here.
